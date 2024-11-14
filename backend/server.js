841710d5a2bd6e7c64021026591e966f748850db
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-11-14 14:07:32 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,15 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready");
-  console.log(process.env.MONGO_URI);
+app.get("/products", async (req, res) => {
+  const product = req.body; //user will send the data
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ sucess: false, messsage: "Please provide all fields" });
+  }
 });
 app.listen(5000, () => {
   connectDB();
-  console.log();
   console.log("server started!! on localhost 5000");
 });
